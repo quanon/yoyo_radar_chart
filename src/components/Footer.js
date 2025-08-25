1171b19d4f3d93a398e82a757eb9d625317edb2d
QUANON <quanon86@gmail.com>
2025-08-25 09:48:12 +0900

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 const Footer = () => (
   <Box display="flex" alignItems="center" justifyContent="center">
     <Typography sx={{ fontSize: '0.6rem' }} color="rgba(0, 0, 0, 0.6)" align="center" gutterBottom>
-      This app is based on <a href="https://www.youtube.com/watch?v=ts6CJosyLHk" target="_blank" rel="noopener noreferrer">this video</a>.
+      This app is based on <a href="https://x.com/Yoyofriends_JP/status/1959570608934535475" target="_blank" rel="noopener noreferrer">this post</a> by Yoyofriends Japan.
       <br />
       Please contact <a href="https://x.com/quanon_jp" target="_blank" rel="noopener noreferrer">@quanon_jp</a> if you have any questions.
     </Typography>
